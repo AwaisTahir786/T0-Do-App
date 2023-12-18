b027272d76594068b6fd873e5254f7ca05ca2ffd
AwaisTahir786 <tahirawais111@gmail.com>
2023-12-18 16:11:16 +0500

    #update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import TodoWrapper from '@/components/todoWrapper'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="bg-[#8758ff] min-h-screen flex items-center justify-center pt-6">
-      <div className='bg-[#1A1A40] py-12 px-8 rounded-r-md '>
+    <main className="h-14 bg-gradient-to-r from-[#8758ffd2] to-[#643cc9]  min-h-screen flex  flex-col items-center justify-center ">
+      
+      <h1 className='text-white font-extrabold text-6xl mb-7'>To-Do App</h1>
+      <div className='bg-[#1A1A40] py-12 px-20 rounded-r-md '>
       <TodoWrapper/>
 
       </div>
